3fd61d9e1a3a5c25dc185ec1e13058e5da9acba6
Shrey Sethia <shreysethia4810@gmail.com>
2026-01-01 12:13:05 +0530

    Learning NEXT-JS

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Shrey Sethia</h1>
+      <p>Interesting person</p>
+      Happy new Year 2026!
+    </>
+  )
+}
+
+export default App
