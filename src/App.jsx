20bfbeef46989809bd1178ec40cb8e712643e3f8
Shrey Sethia <shreysethia4810@gmail.com>
2026-01-01 15:14:01 +0530

    New Features added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card.jsx';
 
 function App() {
 
   return (
     <>
-      <h1>Shrey Sethia</h1>
+      <h1 className="text-blue-600 dark:text-sky-400 border-2 p-4">Shrey Sethia</h1>
       <p>Interesting person</p>
       Happy new Year 2026!
       <p>Wishing you and your family the best year ahead!</p>
+      <Card name="Shrey Sethia"/ >
+      <Card />
+      <Card />
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+
+function Card({name = "Sunny"}){
+    return (
+        <>
+           <h1>{name}</h1>
+        </>
+    )
+}
+
+export default Card;
